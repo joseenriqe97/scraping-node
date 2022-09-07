a608992c885e27df6e91940bcacd64d4296dbb78
joseenriqe97 <joseenriqe92@gmail.com>
2022-09-06 23:07:15 -0400

    WIP

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-const axios = require("axios");
+const puppeteer = require('puppeteer');
 const fs = require("fs");
 
-// URL of the page we want to scrape
-const url = "https://www.carnival.com/cruise-ships.aspx";
+async function initScraping() {
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage()
+    await page.goto('https://www.carnival.com/cruise-ships.aspx')
 
-// Async function which scrapes the data
-async function scrapeData() {
-    try {
-        const { data } = await axios.get(url);
+    const data = await page.evaluate(() => {
+        root = Array.from(document.querySelectorAll("div .text"));
+        cruisesInfo = root.map(r => {
+            if (r.querySelector('ul li')) {
+                return ({
+                    'Name': r.querySelector('h2').textContent,
+                    'Sail To': r.querySelector('ul li').textContent.replace("Sail To: ", ""),
+                    'Sail From': r.querySelector('ul li').nextElementSibling.textContent.replace("Sail From: ", ""),
+                    'Duration': r.querySelector('ul li').nextElementSibling.nextElementSibling.textContent.replace("Duration: ", "")
+                })
+            }
+        });
+        return cruisesInfo;
 
-        const $ = cheerio.load(data);
 
-        const listItems = $('div.activity-result.ship-result')
-        console.log(listItems.html())
+    });
 
-        listItems
-            .each((idx, element) => {
-                // console.log($(element).html())
-                const readTime = $(element).find('div.text').html()
-                console.log(readTime)
-            });
-    } catch (error) {
-        console.log(error)
-    }
+    filterJson = JSON.stringify(data, (k, v) => Array.isArray(v)
+        && !(v = v.filter(e => e)).length ? void 0 : v, 2)
 
+    fs.writeFile("Cruises.json", filterJson, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("Successfully written data to file");
+      });
+
+    await browser.close()
 }
-// Invoke the above function
-scrapeData();
+
+
+initScraping();
